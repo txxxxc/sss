8d13a0e0820dbe2fb4ac0412f23ab7da0d463da6
txxxxc <tomoya113.code@gmail.com>
2022-12-01 12:02:37 +0900

    [update] config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+import path from 'path'
 
+export default defineNuxtConfig({
+  telemetry: false,
+  ssr: false,
+  typescript: {
+    shim: false,
+  },
+  components: [
+    {
+      path: '@/components/',
+      pathPrefix: false,
+    },
+  ],
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.min.css',
+  ],
+  srcDir: 'src/',
+  build: {
+    transpile: ['vuetify'],
+  },
+  alias: {
+    '@': path.resolve(__dirname, 'src'),
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 })
